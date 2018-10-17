e33a9d7afe84cffc82a19e5968a091f4e2849da6
Tony Bargnesi <abargnesi@gmail.com>
2018-10-16 23:23:00 -0400

    add minecraft extension, id: 'minecraft'
    currently it's a skeleton copy of the pen extension

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -8,6 +8,7 @@ const BlockType = require('./block-type');
 // TODO: move these out into a separate repository?
 // TODO: change extension spec so that library info, including extension ID, can be collected through static methods
 const Scratch3PenBlocks = require('../extensions/scratch3_pen');
+const Scratch3MinecraftBlocks = require('../extensions/scratch3_minecraft');
 const Scratch3WeDo2Blocks = require('../extensions/scratch3_wedo2');
 const Scratch3MusicBlocks = require('../extensions/scratch3_music');
 const Scratch3MicroBitBlocks = require('../extensions/scratch3_microbit');
@@ -19,6 +20,7 @@ const Scratch3Ev3Blocks = require('../extensions/scratch3_ev3');
 
 const builtinExtensions = {
     pen: Scratch3PenBlocks,
+    minecraft: Scratch3MinecraftBlocks,
     wedo2: Scratch3WeDo2Blocks,
     music: Scratch3MusicBlocks,
     microbit: Scratch3MicroBitBlocks,
